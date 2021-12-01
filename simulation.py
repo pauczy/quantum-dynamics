b943d41121ec65b13b83125c070954975417f7cf
Paulina Czyz <paula.czyz@gmail.com>
2021-12-01 10:11:13 +0100

    simulation

--- simulation.py
+++ simulation.py
@@ -1,33 +1,32 @@
 import numpy as np
-from sys import argv
 from math import sqrt, sin, pi
+from numba import njit
+from sys import argv
+from time import time
 
 
 def init_psi(n, N):
     xk = np.linspace(0, 1, N+1)
-    psiI = np.zeros(N+1, dtype = np.float64)
-    psiR = np.zeros(N+1, dtype = np.float64)
-    for i, x in enumerate(xk):
-        psiR[i] = sqrt(2) * sin(n*pi*x)
-
-    print(psiR)
-    print(psiI)
+    psiI = np.zeros(N+1)
+    psiR = np.zeros(N+1)
+    for i in range(N+1):
+        psiR[i] = sqrt(2) * sin(n*pi*xk[i])
     return psiI, psiR
 
-
+@njit
 def H_operator(psi, N, k, w, tau):
-    x = np.linspace(0, 1, N+1)
+
+    dx = 1./N
     H = np.zeros(N+1)
     for i in range(1, N):
-        H[i] = -1./2 * (psi[i+1] + psi[i-1] -2*psi[i])/(1./N)**2 + k*(x[i] - 1./2)*psi[i]*sin(w * tau)
-
+        H[i] = -1./2 * (psi[i+1] + psi[i-1] -2*psi[i])/dx**2 + k*(i/N - 1./2)*psi[i]*sin(w * tau)
     return H
 
-def simulate(psiR, psiI, N, k, w, S, dt):
+def simulate(psiR, psiI, N, k, w, S, S_out, S_rho, dt):
     tau = 0
     x = np.linspace(0, 1, N+1)
     dx = 1./N
-    with open(argv[2], 'w+') as out:
+    with open(argv[2], 'w+') as out, open(argv[3], 'w+') as out_rho:
         out.write("t \t N \t x \t E \n")
         for s in range(S):
             psiR = psiR + H_operator(psiI, N, k, w, tau)*dt/2
@@ -36,17 +35,21 @@ def simulate(psiR, psiI, N, k, w, S, dt):
             tau = tau + dt/2
             psiR = psiR + H_operator(psiI, N, k, w, tau)*dt/2
 
-            out.write("{:.3f}".format(tau) + "\t")
-            out.write("{:.3f}".format(dx * np.sum(psiR**2 + psiI**2)) + "\t")
-            out.write("{:.3f}".format(dx * np.sum(x * (np.square(psiR + np.square(psiI))))) + "\t")
-            out.write("{:.3f}".format(dx * np.sum(psiR*H_operator(psiR, N, k, w, tau) + psiI*H_operator(psiI, N, k, w, tau))) + "\n")
+            if (s % S_out == 0):
+                out.write("{:.3f}".format(tau) + "\t")
+                out.write("{:.3f}".format(dx * np.sum(psiR**2 + psiI**2)) + "\t")
+                out.write("{:.3f}".format(dx * np.sum(x * (psiR**2 + psiI**2))) + "\t")
+                out.write("{:.3f}".format(dx * np.sum(psiR*H_operator(psiR, N, k, w, tau) + psiI*H_operator(psiI, N, k, w, tau))) + "\n")
 
-
-
+            if (s % S_rho == 0):
+                rho = psiR[0:N+1:2]**2 + psiI[0:N+1:2]**2
+                np.savetxt(out_rho, rho, delimiter = '\t')
 
 
 
 if __name__ == "__main__":
+
+    tic = time()
 
     params = {}
     with open(argv[1]) as f:
@@ -62,5 +65,7 @@ if __name__ == "__main__":
     print(N)
     print(x)
     print(E)
-    # HR = H_operator(psiR, int(params['N']), params['k'], params['w'], 0.0005 )
-    # simulate(psiR, psiI, int(params['N']), params['k'], params['w'], int(params['S']), params['dt'])
+    simulate(psiR, psiI, int(params['N']), params['k'], params['w'], int(params['S']), int(params['S_out']), int(params['S_rho']), params['dt'])
+
+    toc = time()
+    print('execution time: ', (toc - tic))
